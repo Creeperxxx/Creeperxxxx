3b88bf2a8d6707ba91c8b412338233b69bdf5215
Creeperxxx <157219249+Creeperxxx@users.noreply.github.com>
2024-02-29 18:24:36 +0800

    Add files via upload

--- 树与二叉树.cpp
+++ 树与二叉树.cpp
@@ -1,13 +1,21 @@
 #include <iostream>
 using namespace std;
 #define MAXSIZE 100
+#define TElemType int
 //度：结点子树个数	*树的度：所有结点度的最大值
 
-typedef struct
+//typedef struct
+//{
+//	int data;
+//	void (*print)(TElemType T);
+//}TElemType;
+
+typedef struct BiNode
 {
-	int data;
-	void (*print)(TElemType T);
-}TElemType;
+	TElemType data;
+	struct BiNode* Ichild;
+	struct BiNode* Rchild;
+}BiNode,*Bitree;
 
 typedef struct
 {
@@ -16,14 +24,7 @@ typedef struct
 	int rear;
 }BTQueue;
 
-typedef struct
-{
-	TElemType data;
-	BiNode* Ichild;
-	BiNode* Rchild;
-}BiNode,*Bitree;
-
-typedef struct
+typedef struct TriTNode
 {
 	TElemType data;
 	struct TriTNode* Ichild;
@@ -38,6 +39,56 @@ typedef struct
 	int StackSise;
 }BTStack;
 
+typedef struct BiThrNode//线索二叉树
+{
+	int data;
+	int ilag;
+	int rtag;
+	struct BiThrNode* ichild;
+	struct BiThrNode* rchild;
+}BiThrNode, * BiThrTree;
+
+//树的存储
+typedef struct
+{
+	int data;
+	int parent;
+}PTNode;
+
+typedef struct//找双亲容易
+{
+	PTNode nodes[MAXSIZE];
+	int r;
+	int n;
+}PTree;
+
+typedef struct CTNode
+{
+	int child;
+	struct CTNode* next;
+}CTNode,*ChildPtr;
+
+typedef struct//找孩子容易
+{
+	TElemType data;
+	CTNode* firstchild;
+}CTBox;
+
+typedef struct CSNode //二叉链表存储，即将树转化为二叉树
+{
+	TElemType data;
+	struct CSNode* firstchild;
+	struct CSNode* nextsibling;
+}CSNode, * CSTree;
+
+typedef struct//哈夫曼树结点
+{
+	int weight;
+	int parent;
+	int ich;
+	int rch;
+}HTNode,*HuffmanTree;
+
 void BTStackInit(BTStack& S);
 bool BTStackIsEmpyt(BTStack& S);
 void Push_BTStack(BTStack& S, Bitree T);
@@ -50,10 +101,23 @@ void BTQueueInit(BTQueue& Q);
 bool BTQueueIsEmpty(BTQueue& Q);
 void EnBTQueue(BTQueue& Q,Bitree T);
 void DeBTQueue(BTQueue& Q, Bitree T);
+void CreateBiTree(Bitree& T);
+void CopyBitree(Bitree T, Bitree& NewT);
+int DepthBitree(Bitree T);
+int BiNodeCount(Bitree T);
+int BiNodeLeafCount(Bitree T);
+void CreateHuffmanTree(HuffmanTree& HT, int n);
+void HuffmanSelectMin(HuffmanTree HT, int n, int& S1, int& S2);
+void PrintHuffmanTree(HuffmanTree HT, int n);
 
 int main()
 {
-	return;
+	HuffmanTree HT=NULL;
+	int n = 8;
+	CreateHuffmanTree(HT, n);
+	PrintHuffmanTree(HT, 2 * n - 1);
+	system("pause");
+	return 0;
 }
 
 void BTStackInit(BTStack& S)
@@ -129,7 +193,7 @@ void InOrderTraverse(Bitree T)//栈实现中序遍历
 		}
 		else
 		{
-			Pop_BtStack(S, *p);
+		//	Pop_BtStack(S, *p);
 			visit(p);
 			p = p->Rchild;
 		}
@@ -193,8 +257,170 @@ void DeBTQueue(BTQueue& Q, Bitree T)
 	Q.front = (Q.front + 1) % MAXSIZE;
 }
 
-void print(TElemType T)
+//void print(TElemType T)
+//{
+//	printf("%d", T.data);
+//	return;
+//}
+
+void CreateBiTree(Bitree& T)//先序遍历ABC##DE#G##F###构造二叉树 #表示空结点
 {
-	printf("%d", T.data);
+	char ch;
+	cin >> ch;
+	if (ch == '#')
+	{
+		T = NULL;
+	}
+	else
+	{
+		if (!(T = new BiNode))
+		{
+			exit(OVERFLOW);
+		}
+		/*T->data.data = ch;*/
+		CreateBiTree(T->Ichild);//递归
+		CreateBiTree(T->Rchild);
+	}
+	return;
+}
+
+void CopyBitree(Bitree T, Bitree& NewT)//意义：复制根节点
+{
+	if (T == NULL)
+	{
+		NewT = T;
+		return;
+	}
+	NewT = new BiNode;
+	if (!NewT)
+	{
+		exit(OVERFLOW);
+	}
+	NewT->data = T->data;
+	CopyBitree(T->Ichild, NewT->Ichild);//递归
+	CopyBitree(T->Rchild, NewT->Rchild);
+	return;
+}
+
+int DepthBitree(Bitree T)
+{
+	if (T == NULL)
+	{
+		return 0;
+	}
+	int m;
+	int n;
+	m = DepthBitree(T->Ichild);
+	n = DepthBitree(T->Rchild);
+	return (m > n ? m : n) + 1;
+}
+
+int BiNodeCount(Bitree T)
+{
+	if (T == NULL)
+	{
+		return 0;
+	}
+	return BiNodeCount(T->Ichild) + BiNodeCount(T->Rchild) + 1;
+}
+
+int BiNodeLeafCount(Bitree T)
+{
+	if (T == NULL)
+	{
+		return 0;
+	}
+	if (T->Ichild == NULL && T->Rchild == NULL)
+	{
+		return 1;
+	}
+	return BiNodeLeafCount(T->Ichild) + BiNodeLeafCount(T->Rchild);
+}
+
+void CreateHuffmanTree(HuffmanTree& HT, int n)
+{
+	if (n <= 1)
+	{
+		return;
+	}
+	int m = 2 * n - 1;
+	HT = new HTNode[m+1];
+	for (int i = 1; i <= m; i++)
+	{
+		HT[i].parent = 0;
+		HT[i].ich = 0;
+		HT[i].rch = 0;
+	}
+	
+	for (int i = 1; i <= n; i++)
+	{
+		cin >> HT[i].weight;
+	}//以上为哈夫曼树初始化
+	int min1;
+	int min2;
+	for (int i = n + 1; i <= m; i++)
+	{
+		HuffmanSelectMin(HT, i-1, min1, min2);
+		HT[i].weight = HT[min1].weight + HT[min2].weight;
+		HT[i].ich = min1;
+		HT[i].rch = min2;
+		HT[min1].parent = i;
+		HT[min2].parent = i;
+	}
+	return;
+}
+
+void HuffmanSelectMin(HuffmanTree HT, int n, int& S1, int& S2)
+{
+	int index = 0;
+	S1 = 0;
+	S2 = 0;
+	for (int i = 1; i <= n; i++)
+	{
+		if (HT[i].parent != 0)
+		{
+			continue;
+		}
+		if (index == 0)
+		{
+			S1 = i;
+			index++;
+			continue;
+		}
+		if (index == 1)
+		{
+			if (HT[i].weight < HT[S1].weight)
+			{
+				S2 = S1;
+				S1 = i;
+			}
+			else
+			{
+				S2 = i;
+			}
+			index++;
+			continue;
+		}
+		if (HT[i].weight <= HT[S1].weight)
+		{
+			S2 = S1;
+			S1 = i;
+		}
+		else if (HT[i].weight<HT[S2].weight)
+		{
+			S2 = i;
+		}
+	}
+}
+
+void PrintHuffmanTree(HuffmanTree HT, int n)
+{
+	int a = 4;
+	cout << "index\t" << "weight\t" << "parent\t"<<"ich\t" << "rch\t" << endl;
+	for (int i = 1; i <= n; i++)
+	{
+		cout << i << "\t" << HT[i].weight << "\t" << HT[i].parent
+			<<"\t"<<HT[i].ich << "\t"<< HT[i].rch << "\t" << endl;
+	}
 	return;
 }
